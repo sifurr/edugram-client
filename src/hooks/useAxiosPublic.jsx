8d6a9ad27fb90459da014045871142b73629e199
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-12-03 18:52:28 +0600

    build(config): replace the remote server domain issue

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const axiosPublic = axios.create({    
-    baseURL: `http://localhost:5000`
-    // baseURL: `https://student-and-class-management-server-one.vercel.app`
+const axiosPublic = axios.create({        
+    baseURL: `https://student-and-class-management-server-one.vercel.app`
 })
 
 const useAxiosPublic = () => {
